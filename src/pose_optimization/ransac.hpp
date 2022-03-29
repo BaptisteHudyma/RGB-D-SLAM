7e9b71a59b27f735496cd8e0e494e943471488ee
BaptisteHudyma <bapt.hudyma95650@gmail.com>
2022-03-29 23:34:35 +0200

    Clean ransac selection code

--- src/pose_optimization/ransac.hpp
+++ src/pose_optimization/ransac.hpp
@@ -6,25 +6,28 @@ namespace rgbd_slam {
     namespace pose_optimization {
 
         /**
-         * \brief Return a random subset of matches, of size n. It is inefficient for if n is very inferior to matchedPoints.size
+         * \brief Return a random subset of unique elements, of size n. It is inefficient if n is very inferior to inContainer.size
+         * \param[in] inContainer A container of size > numberOfElementsToChoose, in which this function will pick elements
+         * \param[in] numberOfElementsToChoose The number of elements we which to find in the final container
+         * \return A container of size numberOfElementsToChoose, with no duplicated elements.
          */
         template< template<typename> class Container, typename T>
-            Container<T> get_n_random_matches(const Container<T>& inContainer, const uint n)
+            Container<T> get_random_subset(const Container<T>& inContainer, const uint numberOfElementsToChoose)
             {
                 const size_t maxIndex = inContainer.size();
-                assert(n <= maxIndex);
+                assert(numberOfElementsToChoose <= maxIndex);
 
-                // get a reference vector and shuffle it
+                // get a vector of references and shuffle it
                 std::vector<std::reference_wrapper<const T>> copyVector(inContainer.cbegin(), inContainer.cend());
                 std::random_shuffle(copyVector.begin(), copyVector.end());
 
                 assert(copyVector.size() == inContainer.size());
 
                 // copy the first matches, they will be randoms
-                Container<T> selectedMatches;
-                for(uint i = 0; i < n; ++i)
-                    selectedMatches.insert(selectedMatches.begin(), copyVector[i]);
-                return selectedMatches;
+                Container<T> outContainer;
+                for(uint i = 0; i < numberOfElementsToChoose; ++i)
+                    outContainer.insert(outContainer.begin(), copyVector[i]);
+                return outContainer;
             }
 
     }   /* pose_optimization */
