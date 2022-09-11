2b42b6fe958e430dde4b66450b3c467903ea2005
BaptisteHudyma <bapt.hudyma95650@gmail.com>
2022-09-11 19:07:28 +0200

    Use correct namespace

--- src/utils/kalman_filter.cpp
+++ src/utils/kalman_filter.cpp
@@ -3,47 +3,53 @@
 
 #include "kalman_filter.hpp"
 
-KalmanFilter::KalmanFilter(const Eigen::MatrixXd& systemDynamics,
-                            const Eigen::MatrixXd& outputMatrix,
-                            const Eigen::MatrixXd& processNoiseCovariance,
-                            const Eigen::MatrixXd& measurementNoiseCovariance):
-        systemDynamics(systemDynamics), outputMatrix(outputMatrix), 
-        processNoiseCovariance(processNoiseCovariance), measurementNoiseCovariance(measurementNoiseCovariance),
-        measurementDimension(outputMatrix.rows()), stateDimension(systemDynamics.rows()),
-        isInitialized(false),
-        I(stateDimension, stateDimension), stateEstimate(stateDimension)
-{
-    I.setIdentity();
-}
+namespace rgbd_slam {
+    namespace utils {
 
-void KalmanFilter::init(const Eigen::MatrixXd& firstEstimateErrorCovariance, const Eigen::VectorXd& x0) {
-    estimateErrorCovariance = firstEstimateErrorCovariance;
-    stateEstimate = x0;
-    isInitialized = true;
-}
+        KalmanFilter::KalmanFilter(const Eigen::MatrixXd& systemDynamics,
+                                    const Eigen::MatrixXd& outputMatrix,
+                                    const Eigen::MatrixXd& processNoiseCovariance,
+                                    const Eigen::MatrixXd& measurementNoiseCovariance):
+                systemDynamics(systemDynamics), outputMatrix(outputMatrix), 
+                processNoiseCovariance(processNoiseCovariance), measurementNoiseCovariance(measurementNoiseCovariance),
+                measurementDimension(outputMatrix.rows()), stateDimension(systemDynamics.rows()),
+                isInitialized(false),
+                I(stateDimension, stateDimension), stateEstimate(stateDimension)
+        {
+            I.setIdentity();
+        }
 
-void KalmanFilter::update(const Eigen::VectorXd& newMeasurement) {
+        void KalmanFilter::init(const Eigen::MatrixXd& firstEstimateErrorCovariance, const Eigen::VectorXd& x0) {
+            estimateErrorCovariance = firstEstimateErrorCovariance;
+            stateEstimate = x0;
+            isInitialized = true;
+        }
 
-    if(not is_initialized())
-        throw std::runtime_error("Filter is not isInitialized!");
+        void KalmanFilter::update(const Eigen::VectorXd& newMeasurement) {
 
-    // Get new raw estimate
-    Eigen::VectorXd newStateEstimate = systemDynamics * stateEstimate;
-    estimateErrorCovariance = systemDynamics * estimateErrorCovariance * systemDynamics.transpose() + processNoiseCovariance;
-    
-    // compute Kalman gain
-    const Eigen::MatrixXd kalmanGain = estimateErrorCovariance * outputMatrix.transpose() * (outputMatrix * estimateErrorCovariance * outputMatrix.transpose() + measurementNoiseCovariance).inverse();
-    
-    // update the estimate with covariance
-    newStateEstimate += kalmanGain * (newMeasurement - outputMatrix * newStateEstimate);
+            if(not is_initialized())
+                throw std::runtime_error("Filter is not isInitialized!");
 
-    // update the covariance and state estimation
-    estimateErrorCovariance = (I - kalmanGain * outputMatrix) * estimateErrorCovariance;
-    stateEstimate = newStateEstimate;
-}
+            // Get new raw estimate
+            Eigen::VectorXd newStateEstimate = systemDynamics * stateEstimate;
+            estimateErrorCovariance = systemDynamics * estimateErrorCovariance * systemDynamics.transpose() + processNoiseCovariance;
+            
+            // compute Kalman gain
+            const Eigen::MatrixXd kalmanGain = estimateErrorCovariance * outputMatrix.transpose() * (outputMatrix * estimateErrorCovariance * outputMatrix.transpose() + measurementNoiseCovariance).inverse();
+            
+            // update the estimate with covariance
+            newStateEstimate += kalmanGain * (newMeasurement - outputMatrix * newStateEstimate);
 
-void KalmanFilter::update(const Eigen::VectorXd& newMeasurement, const Eigen::MatrixXd& systemDynamics) {
+            // update the covariance and state estimation
+            estimateErrorCovariance = (I - kalmanGain * outputMatrix) * estimateErrorCovariance;
+            stateEstimate = newStateEstimate;
+        }
 
-    this->systemDynamics = systemDynamics;
-    update(newMeasurement);
+        void KalmanFilter::update(const Eigen::VectorXd& newMeasurement, const Eigen::MatrixXd& systemDynamics) {
+
+            this->systemDynamics = systemDynamics;
+            update(newMeasurement);
+        }
+ 
+    }
 }
--- src/utils/kalman_filter.hpp
+++ src/utils/kalman_filter.hpp
@@ -4,75 +4,79 @@
 
 #include <Eigen/Dense>
 
-class KalmanFilter {
+namespace rgbd_slam {
+    namespace utils {
 
-public:
+        class KalmanFilter {
 
-    /**
-    * \brief Create a Kalman filter with the specified matrices.
-    * \param[in] systemDynamics System dynamics matrix
-    * \param[in] outputMatrix Output matrix
-    * \param[in] processNoiseCovariance Process noise covariance
-    * \param[in] measurementNoiseCovariance Measurement noise covariance
-    */
-    KalmanFilter(
-            const Eigen::MatrixXd& systemDynamics,
-            const Eigen::MatrixXd& outputMatrix,
-            const Eigen::MatrixXd& processNoiseCovariance,
-            const Eigen::MatrixXd& measurementNoiseCovariance
-    );
+        public:
 
-    /**
-    * \brief Initialize the filter with a guess for initial states
-    * \param[in] firstEstimateErrorCovariance Estimate error covariance for the first guess
-    * \param[in] x0 The original state estimate
-    */
-    void init(const Eigen::MatrixXd& firstEstimateErrorCovariance, const Eigen::VectorXd& x0 );
+            /**
+            * \brief Create a Kalman filter with the specified matrices.
+            * \param[in] systemDynamics System dynamics matrix
+            * \param[in] outputMatrix Output matrix
+            * \param[in] processNoiseCovariance Process noise covariance
+            * \param[in] measurementNoiseCovariance Measurement noise covariance
+            */
+            KalmanFilter(
+                    const Eigen::MatrixXd& systemDynamics,
+                    const Eigen::MatrixXd& outputMatrix,
+                    const Eigen::MatrixXd& processNoiseCovariance,
+                    const Eigen::MatrixXd& measurementNoiseCovariance
+            );
 
-    /**
-    * \brief Update the estimated state based on measured values. The time step is assumed to remain constant.
-    * \param[in] newMeasurement new measurement
-    */
-    void update(const Eigen::VectorXd& newMeasurement);
+            /**
+            * \brief Initialize the filter with a guess for initial states
+            * \param[in] firstEstimateErrorCovariance Estimate error covariance for the first guess
+            * \param[in] x0 The original state estimate
+            */
+            void init(const Eigen::MatrixXd& firstEstimateErrorCovariance, const Eigen::VectorXd& x0 );
 
-    /**
-    * \brief Update the estimated state based on measured values, using the given time step and dynamics matrix.
-    * \param[in] newMeasurement new measurement
-    * \param[in] systemDynamics systemDynamics new system dynamics matrix 
-    */
-    void update(const Eigen::VectorXd& y, const Eigen::MatrixXd& systemDynamics);
+            /**
+            * \brief Update the estimated state based on measured values. The time step is assumed to remain constant.
+            * \param[in] newMeasurement new measurement
+            */
+            void update(const Eigen::VectorXd& newMeasurement);
+
+            /**
+            * \brief Update the estimated state based on measured values, using the given time step and dynamics matrix.
+            * \param[in] newMeasurement new measurement
+            * \param[in] systemDynamics systemDynamics new system dynamics matrix 
+            */
+            void update(const Eigen::VectorXd& y, const Eigen::MatrixXd& systemDynamics);
 
 
-    Eigen::VectorXd get_state() const {
-        return stateEstimate;
-    };
+            Eigen::VectorXd get_state() const {
+                return stateEstimate;
+            };
 
-    bool is_initialized() const {
-        return isInitialized;
+            bool is_initialized() const {
+                return isInitialized;
+            }
+
+        private:
+
+            // Matrices for computation
+            Eigen::MatrixXd systemDynamics;
+            const Eigen::MatrixXd outputMatrix;
+            const Eigen::MatrixXd processNoiseCovariance;
+            const Eigen::MatrixXd measurementNoiseCovariance;
+
+            // System dimensions
+            const size_t measurementDimension;
+            const size_t stateDimension;
+
+            // Is the filter isInitialized?
+            bool isInitialized;
+
+            // stateDimension-size identity
+            Eigen::MatrixXd I;
+
+            // Estimated state
+            Eigen::VectorXd stateEstimate;
+            Eigen::MatrixXd estimateErrorCovariance;
+        };
     }
-
-private:
-
-    // Matrices for computation
-    Eigen::MatrixXd systemDynamics;
-    const Eigen::MatrixXd outputMatrix;
-    const Eigen::MatrixXd processNoiseCovariance;
-    const Eigen::MatrixXd measurementNoiseCovariance;
-
-    // System dimensions
-    const size_t measurementDimension;
-    const size_t stateDimension;
-
-    // Is the filter isInitialized?
-    bool isInitialized;
-
-    // stateDimension-size identity
-    Eigen::MatrixXd I;
-
-    // Estimated state
-    Eigen::VectorXd stateEstimate;
-    Eigen::MatrixXd estimateErrorCovariance;
-};
-
+}
 
 #endif
