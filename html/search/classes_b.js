ec69d6b4518f7c0a74e7a4abdf3b739fd5e20f08
BaptisteHudyma <bapt.hudyma95650@gmail.com>
2022-10-13 19:55:34 +0200

    Update documentation

--- html/search/classes_b.js
+++ html/search/classes_b.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['screencoordinates_0',['screenCoordinates',['../structrgbd__slam_1_1screen_coordinates.html',1,'rgbd_slam']]],
-  ['staged_5fpoint_1',['Staged_Point',['../classrgbd__slam_1_1map__management_1_1_staged___point.html',1,'rgbd_slam::map_management']]]
+  ['sharedkalmanfilter_1',['SharedKalmanFilter',['../classrgbd__slam_1_1tracking_1_1_shared_kalman_filter.html',1,'rgbd_slam::tracking']]],
+  ['staged_5fpoint_2',['Staged_Point',['../classrgbd__slam_1_1map__management_1_1_staged___point.html',1,'rgbd_slam::map_management']]]
 ];
