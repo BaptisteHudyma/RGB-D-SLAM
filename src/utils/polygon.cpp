847320e422e09c5ccc1e7afc17e219f46404b3c9
BaptisteHudyma <bapt.hudyma95650@gmail.com>
2023-04-04 16:42:08 +0200

    Add is_point_in_polygon function

--- src/utils/polygon.cpp
+++ src/utils/polygon.cpp
@@ -25,7 +25,14 @@ std::pair<vector3, vector3> get_plane_coordinate_system(const vector3& normal)
     return std::make_pair(u, v);
 }
 
-Polygon::Polygon(const std::vector<vector2>& points) : _boundaryPoints(points) {}
+Polygon::Polygon(const std::vector<vector2>& points,
+                 const vector2& lowerLeftBoundary,
+                 const vector2& upperRightBoundary) :
+    _boundaryPoints(points),
+    _lowerLeftBoundary(lowerLeftBoundary),
+    _upperRightBoundary(upperRightBoundary)
+{
+}
 
 /**
  * \brief Rotate the given vetor by 90 degrees
@@ -36,7 +43,7 @@ Polygon Polygon::compute_convex_hull(const std::vector<vector2>& pointsIn)
 {
     if (pointsIn.size() < 3)
     {
-        outputs::log_warning("Could not find boundary for plane patch");
+        outputs::log_warning("Cannot compute a polygon with less than 3 sides");
         return Polygon();
     }
     std::vector<vector2> sortedPoints(pointsIn);
@@ -62,20 +69,56 @@ Polygon Polygon::compute_convex_hull(const std::vector<vector2>& pointsIn)
         return dir > 0;
     }); // Sort the points by angle to the chosen first point
 
-    std::vector<vector2> result;
+    vector2 lowerLeftBoundary = vector2::Zero();
+    vector2 upperRightBoundary = vector2::Zero();
+    std::vector<vector2> boundary;
     for (const vector2& point: sortedPoints)
     {
         // For as long as the last 3 points cause the hull to be non-convex, discard the middle one
-        while (result.size() >= 2 && (rotate90(result[result.size() - 1] - result[result.size() - 2]))
-                                                     .dot(point - result[result.size() - 1]) <= 0)
+        while (boundary.size() >= 2 && (rotate90(boundary[boundary.size() - 1] - boundary[boundary.size() - 2]))
+                                                       .dot(point - boundary[boundary.size() - 1]) <= 0)
         {
-            result.pop_back();
+            boundary.pop_back();
         }
-        result.push_back(point);
+        boundary.push_back(point);
+
+        lowerLeftBoundary.x() = std::min(lowerLeftBoundary.x(), point.x());
+        lowerLeftBoundary.y() = std::min(lowerLeftBoundary.y(), point.y());
+        upperRightBoundary.x() = std::max(upperRightBoundary.x(), point.x());
+        upperRightBoundary.y() = std::max(upperRightBoundary.y(), point.y());
     }
-    return Polygon(result);
+    return Polygon(boundary, lowerLeftBoundary, upperRightBoundary);
 }
 
-void Polygon::merge(const Polygon& other) {}
+bool Polygon::contains(const vector2& point) const
+{
+    // Source: Wm. Randolph Franklin
+
+    const double pointX = point.x();
+    const double pointY = point.y();
+
+    // Check that the point is inside the coarse boundary for efficiency
+    /*if (p.x() < _Xmin or p.x() > _Xmax or p.y() < _Ymin or p.y() > _Ymax)
+        return false;
+    */
+
+    bool res = false;
+    // i form 0 to n-1, j = i-1
+    for (uint i = 0, j = _boundaryPoints.size() - 1; i < _boundaryPoints.size(); j = i++)
+    {
+        const vector2& vertex1 = _boundaryPoints[i];
+        const vector2& vertex2 = _boundaryPoints[j];
+
+        if (((vertex1.y() > pointY) != (vertex2.y() > pointY)) and
+            (pointX < (vertex2.x() - vertex1.x()) * (pointY - vertex1.y()) / (vertex2.y() - vertex1.y()) + vertex1.x()))
+            res = !res;
+    }
+    return res;
+}
+
+void Polygon::merge(const Polygon& other)
+{
+    // TODO
+}
 
 } // namespace rgbd_slam::utils
--- src/utils/polygon.hpp
+++ src/utils/polygon.hpp
@@ -18,7 +18,7 @@ class Polygon
 {
   public:
     Polygon() = default;
-    Polygon(const std::vector<vector2>& points);
+    Polygon(const std::vector<vector2>& points, const vector2& lowerLeftBoundary, const vector2& upperRightBoundary);
 
     /**
      * \brief Compute the convex hull for a set of points
@@ -26,6 +26,11 @@ class Polygon
      * \return The ordered points defining a convex hull of points
      */
     static Polygon compute_convex_hull(const std::vector<vector2>& points);
+
+    /**
+     * \brief Return true if this point is in the polygon boundaries
+     */
+    bool contains(const vector2& point) const;
 
     /**
      * merge the other polygon into this one
@@ -37,6 +42,10 @@ class Polygon
 
   private:
     std::vector<vector2> _boundaryPoints;
+
+    // represent the two sides of the rectangle englobing this polygon
+    vector2 _lowerLeftBoundary = vector2::Zero();
+    vector2 _upperRightBoundary = vector2::Zero();
 };
 
 } // namespace rgbd_slam::utils
