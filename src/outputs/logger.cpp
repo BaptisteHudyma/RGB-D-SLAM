65df46994faa49a4eead68e80eb4db70e16f54a8
BaptisteHudyma <bapt.hudyma95650@gmail.com>
2023-10-24 20:33:57 +0200

    Add colors to output, and choose debug level

--- src/outputs/logger.cpp
+++ src/outputs/logger.cpp
@@ -4,29 +4,54 @@
 
 namespace rgbd_slam::outputs {
 
+enum class InfoLevel
+{
+    ALL = 0,  // display all logs
+    LOW = 1,  // show logs low and above
+    MED = 2,  // show logs medium and above
+    HIGH = 3, // show logs high and above
+
+    NONE = 100, // deactivate all logs
+};
+
+// prevent too much logging
+constexpr InfoLevel INFO_LEVEL = InfoLevel::ALL;
+
 void log(const std::string_view& message, const std::source_location& location) noexcept
 {
-    std::cout << "[INF] " << std::filesystem::path(location.file_name()).filename().string() << "(" << location.line()
-              << ":" << location.column()
-              << ") "
-              //<< location.function_name() << " | "
-              << message << std::endl;
+    if (INFO_LEVEL <= InfoLevel::LOW)
+    {
+        // display in blue
+        std::cout << "\x1B[34m[INF] " << std::filesystem::path(location.file_name()).filename().string() << "("
+                  << location.line() << ":" << location.column()
+                  << ") "
+                  //<< location.function_name() << " | "
+                  << message << "\033[0m" << std::endl;
+    }
 }
 void log_warning(const std::string_view& message, const std::source_location& location) noexcept
 {
-    std::cerr << "[WARN] " << std::filesystem::path(location.file_name()).filename().string() << "(" << location.line()
-              << ":" << location.column()
-              << ") "
-              //<< location.function_name() << " | "
-              << message << std::endl;
+    if (INFO_LEVEL <= InfoLevel::MED)
+    {
+        // display in yellow
+        std::cerr << "\x1B[33m[WARN] " << std::filesystem::path(location.file_name()).filename().string() << "("
+                  << location.line() << ":" << location.column()
+                  << ") "
+                  //<< location.function_name() << " | "
+                  << message << "\033[0m" << std::endl;
+    }
 }
 void log_error(const std::string_view& message, const std::source_location& location) noexcept
 {
-    std::cerr << "[ERR] " << std::filesystem::path(location.file_name()).filename().string() << "(" << location.line()
-              << ":" << location.column()
-              << ") "
-              //<< location.function_name() << " | "
-              << message << std::endl;
+    if (INFO_LEVEL <= InfoLevel::HIGH)
+    {
+        // display in red
+        std::cerr << "\x1B[31m[ERR] " << std::filesystem::path(location.file_name()).filename().string() << "("
+                  << location.line() << ":" << location.column()
+                  << ") "
+                  //<< location.function_name() << " | "
+                  << message << "\033[0m" << std::endl;
+    }
 }
 
 } // namespace rgbd_slam::outputs
