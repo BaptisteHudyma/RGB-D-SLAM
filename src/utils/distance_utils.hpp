994387009cc7eabd2db476fbbb7ec96dfd46e048
BaptisteHudyma <bapt.hudyma95650@gmail.com>
2023-12-24 02:24:15 +0100

    split utils function for line distance

--- src/utils/distance_utils.hpp
+++ src/utils/distance_utils.hpp
@@ -28,13 +28,16 @@ namespace rgbd_slam::utils {
 /**
  * \brief compute the distance between two lines.
  * This is the distance between the two points closest to each others on each line
+ * \param[out] closestL1toL2 The closest point on L1 to L2
+ * \param[out] closestL2toL1 The closest point on L2 to L1
  * \return a signed distance, in the same unit as the points
  */
-template<int Dim>
-[[nodiscard]] Eigen::Vector<double, Dim> signed_line_distance(const Eigen::Vector<double, Dim>& line1point,
-                                                              const Eigen::Vector<double, Dim>& line1normal,
-                                                              const Eigen::Vector<double, Dim>& line2point,
-                                                              const Eigen::Vector<double, Dim>& line2normal) noexcept
+template<int Dim> [[nodiscard]] bool compute_closest_points(const Eigen::Vector<double, Dim>& line1point,
+                                                            const Eigen::Vector<double, Dim>& line1normal,
+                                                            const Eigen::Vector<double, Dim>& line2point,
+                                                            const Eigen::Vector<double, Dim>& line2normal,
+                                                            Eigen::Vector<double, Dim>& closestL1toL2,
+                                                            Eigen::Vector<double, Dim>& closestL2toL1) noexcept
 {
     // we want to find a point that the two lines pass by, define by the origin of the line and the normal :
     // P = p1 + t1 * d1
@@ -45,7 +48,7 @@ template<int Dim>
     if (n.isApproxToConstant(0.0))
     {
         // parallel line distance
-        return line1normal.cross(line1point - line2point);
+        return false;
     }
 
     const Eigen::Vector<double, Dim>& n1 = line1normal.cross(n);
@@ -54,9 +57,30 @@ template<int Dim>
     const double t1 = (line2point - line1point).dot(n2) / line1normal.dot(n2);
     const double t2 = (line1point - line2point).dot(n1) / line2normal.dot(n1);
 
-    const Eigen::Vector<double, Dim> c1 = line1point + t1 * line1normal; // closest point to line 2 on line 1
-    const Eigen::Vector<double, Dim> c2 = line2point + t2 * line2normal; // closest point to line 1 on line 2
-    return c1 - c2;
+    closestL1toL2 = line1point + t1 * line1normal; // closest point to line 2 on line 1
+    closestL2toL1 = line2point + t2 * line2normal; // closest point to line 1 on line 2
+    return true;
+}
+
+/**
+ * \brief compute the distance between two lines.
+ * This is the distance between the two points closest to each others on each line
+ * \return a signed distance, in the same unit as the points
+ */
+template<int Dim>
+[[nodiscard]] Eigen::Vector<double, Dim> signed_line_distance(const Eigen::Vector<double, Dim>& line1point,
+                                                              const Eigen::Vector<double, Dim>& line1normal,
+                                                              const Eigen::Vector<double, Dim>& line2point,
+                                                              const Eigen::Vector<double, Dim>& line2normal) noexcept
+{
+    Eigen::Vector<double, Dim> closestL1toL2;
+    Eigen::Vector<double, Dim> closestL2toL1;
+    if (compute_closest_points<Dim>(line1point, line1normal, line2point, line2normal, closestL1toL2, closestL2toL1))
+    {
+        return closestL1toL2 - closestL2toL1;
+    }
+
+    return line1normal.cross(line1point - line2point);
 }
 
 } // namespace rgbd_slam::utils
