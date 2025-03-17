f4d9202c28752f03d2f7286b004f550e6300f092
BaptHdm <bapt.hudyma95650@gmail.com>
2025-03-17 10:43:56 +0100

    change optimization feature pointer to reference

--- src/pose_optimization/levenberg_marquardt_functors.hpp
+++ src/pose_optimization/levenberg_marquardt_functors.hpp
@@ -55,9 +55,9 @@ vector6 get_optimization_coefficient_from_pose(const utils::PoseBase& pose);
  * \param[in] optimizationCoefficients The coefficients to transform back to pose
  * \return The pose obtained from the coefficients
  */
-utils::PoseBase get_pose_from_optimization_coeffiencients(const vector6& optimizationCoefficients);
-utils::PoseBase get_pose_from_optimization_coeffiencients(const vector6& optimizationCoefficients,
-                                                          Eigen::Matrix<double, 7, 6>& jacobian);
+utils::PoseBase get_pose_from_optimization_coefficients(const vector6& optimizationCoefficients);
+utils::PoseBase get_pose_from_optimization_coefficients(const vector6& optimizationCoefficients,
+                                                        Eigen::Matrix<double, 7, 6>& jacobian);
 
 /**
  * \brief Implementation of the main pose and orientation optimisation, to be used by the Levenberg Marquard
@@ -70,7 +70,7 @@ struct Global_Pose_Estimator : Levenberg_Marquardt_Functor<double>
      * \param[in] optimizationParts The sum of all feature parts
      * \param[in] features The container for the matched features
      */
-    Global_Pose_Estimator(const size_t optimizationParts, const matches_containers::match_container* const features);
+    Global_Pose_Estimator(const size_t optimizationParts, const matches_containers::match_container& features);
 
     /**
      * \brief Implementation of the objective function
@@ -83,7 +83,8 @@ struct Global_Pose_Estimator : Levenberg_Marquardt_Functor<double>
   private:
     // use pointers to prevent useless copy
     const size_t _optimizationParts;
-    const matches_containers::match_container* const _features;
+    // const reference to the feature object, that MUST NEVER be updated during the optimization
+    const matches_containers::match_container& _features;
 };
 
 struct Global_Pose_Functor : Eigen::NumericalDiff<Global_Pose_Estimator>
